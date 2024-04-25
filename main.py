dd967119892b51768a5a0e58a7574226fd573ff8
alexxgusse <@ALEXANDRGUSEVgithub>
2024-04-25 10:11:35 +0500

    Внесены изменения в README.md. Изменена логика функции

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def convert_inches_to_sm(height_inches):
 
 
 # Функция для перевода фунтов в килограммы
-def convert_ibs_kg(weight_lbs):
+def convert_ibs_to_kg(weight_lbs):
     return round(weight_lbs * 0.453592)
 
 
@@ -20,16 +20,17 @@ def csv_to_json(csvFilePath, jsonFilePath):
 
         for row in reader:
             if all(row.values()):
-                row = {k.strip('"').strip(): v.strip('"').strip() for k, v in row.items()}
+                processed_row = {k.strip('"').strip(): v.strip('"').strip() for k, v in row.items()}
 
-                row.pop("Team", None)
+                processed_row.pop("Team", None)
 
-                row['Height'] = convert_inches_to_sm(float(row.pop('Height(inches)')))
-                row['Weight'] = convert_ibs_kg(float(row.pop('Weight(ibs)')))
+                processed_row['Height'] = convert_inches_to_sm(float(processed_row.pop('Height(inches)')))
 
-                row['Age'] = int(float(row['Age']))
+                processed_row['Weight'] = convert_ibs_to_kg(float(processed_row.pop('Weight(ibs)')))
 
-                data.append(row)
+                processed_row['Age'] = int(float(processed_row['Age']))
+
+                data.append(processed_row)
 
     with open(jsonFilePath, 'w') as jsonfile:
         json.dump(data, jsonfile, indent=1)
